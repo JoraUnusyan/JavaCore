201c02f2f15be20a197330a6e20932747e1457a5
Jora Unusyan <joraunusyan1@gmail.com>
2024-08-03 04:46:58 +0400

    5 առաջադրանքն

--- src/homework/homework4/Sort.java
+++ src/homework/homework4/Sort.java
@@ -2,12 +2,13 @@ package homework.homework4;
 
 public class Sort {
     void distribution() {
-        int[] array = {4, 7, 1, 3, 9, 0, 2};
+        int[] array = {4, 7, 7, 1, 3, 9, 0, 2};
         int tempStorage = 0;
         boolean proof;
         for (int i = 0; i < array.length; i++) {
+            proof = false;
             for (int j = 0; j < array.length; j++) {
-                proof = false;
+
                 if (array[i] <= array[j]) {
                     tempStorage = array[i];
                     array[i] = array[j];
